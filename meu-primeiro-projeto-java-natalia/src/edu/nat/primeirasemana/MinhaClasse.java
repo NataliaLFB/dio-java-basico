38c03f9598ae165e63b82da31d642cadf872cab6
Natalia Luz F B <natalialuzcbpf@gmail.com>
2024-05-21 15:33:20 -0300

    adicionei comentário teste

--- meu-primeiro-projeto-java-natalia/src/edu/nat/primeirasemana/MinhaClasse.java
+++ meu-primeiro-projeto-java-natalia/src/edu/nat/primeirasemana/MinhaClasse.java
@@ -14,6 +14,6 @@ public class MinhaClasse {
     public static String nomeCompleto (String primeiroNome, String segundoNome) {
         return primeiroNome.concat(" ").concat(segundoNome);
     }
-
+//trabalho curso xxx
 }
  
